5e5615e2dc3a04dc42b3e386f2fdf8e39d1d74f2
Faril Mammadov <memmedovfaril@gmail.com>
2022-12-24 20:50:57 +0400

    v1.0

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,13 +1,11 @@
 $('.banner__carousel').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToShow: 2,
+    slidesToScroll: 2,
     autoplay: true,
     centerPadding: '20px',
     arrows: false,
-    dots: false,
+    dots: true,
     responsive: [
       {
         breakpoint:1280,
@@ -50,3 +48,54 @@ $('.banner__carousel').slick({
       },
     ]
   });
+
+  // $('.category__carousel').slick({
+  //   infinite: true,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 6,
+  //   autoplay: true,
+  //   centerPadding: '20px',
+  //   arrows: false,
+  //   dots: false,
+  //   responsive: [
+  //     {
+  //       breakpoint:1280,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint:992,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 768,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1,
+  //       },
+  //     },  
+  //     {
+  //       breakpoint:480,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1,
+  //         centerMode: true,
+  //         dots: false,
+  //       },
+  //     },
+  //     {
+  //       breakpoint:360,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1,
+  //         centerMode: true,
+  //         dots: false,
+  //       },
+  //     },
+  //   ]
+  // });
