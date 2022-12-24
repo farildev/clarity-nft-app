b887e7c1b47f08cff51e000de12c12e973582e75
Faril Mammadov <memmedovfaril@gmail.com>
2022-12-24 22:43:20 +0400

    v1.0

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -49,6 +49,56 @@ $('.banner__carousel').slick({
       },
     ]
   });
+  $('.seller__carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    centerPadding: '20px',
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint:1280,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint:992,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+        },
+      },  
+      {
+        breakpoint:480,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          centerMode: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint:360,
+        settings: {
+          slidesToScroll: 1,
+          slidesToShow: 1,
+          centerMode: true,
+          dots: false,
+        },
+      },
+    ]
+  });
 
   // $('.category__carousel').slick({
   //   infinite: true,
